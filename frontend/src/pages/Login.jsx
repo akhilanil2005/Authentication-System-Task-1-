64229570d787b70a982e1b2d4c61f2edf480573e
akhilanil2005 <akhil.richinnovations@gmail.com>
2026-06-20 00:02:32 +0530

    Update API URL

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/login",
+        "https://authentication-system-2201.onrender.com/login",
         {
           email,
           password,
